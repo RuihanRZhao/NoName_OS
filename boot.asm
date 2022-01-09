44a671c5b3cf2d5cf3313d40af1e233907dda69b
RyenBZhao <fouxzhao@GMAIL.COM>
2022-01-09 07:57:43 +0000

    boot complete

--- boot.asm
+++ boot.asm
@@ -1,4 +1,8 @@
-jmp $ 
-
-times 510-($-$$) db 0 
-db 0x55, 0xaa
+; Here is $$
+jmp $; where $ is the current memory address
+; Line-2 is $
+times 510 - ($ - $$) db 0 
+; $$ = Beginning of current section
+; ($ - $$) = length of previous code = current address - section start = 3
+; n+510-n=510 bytes
+db 0x55, 0xaa ; 2 bytes
